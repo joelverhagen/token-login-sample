29429bd5e6cd671c5cb482bd2ad79fdb5b5de511
Joel Verhagen <jver@microsoft.com>
2023-12-04 14:31:16 -0500

    Add sample tool

---
+++ src/SampleTool/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+var versionString = Assembly
+    .GetEntryAssembly()?
+    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+    .InformationalVersion
+    .ToString();
+
+Console.WriteLine($"Running version: {versionString ?? "(unknown)"}");
